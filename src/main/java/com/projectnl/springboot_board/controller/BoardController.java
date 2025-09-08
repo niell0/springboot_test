32eaa34a339d6a934bef119469fb4453ae4c24b4
niel0001 <inven714@naver.com>
2025-09-08 13:13:14 +0900

    조회 기능 추가

--- src/main/java/com/projectnl/springboot_board/controller/BoardController.java
+++ src/main/java/com/projectnl/springboot_board/controller/BoardController.java
@@ -3,17 +3,37 @@ package com.projectnl.springboot_board.controller;
 import com.projectnl.springboot_board.dto.BoardDTO;
 import com.projectnl.springboot_board.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 
 public class BoardController {
     private final BoardService boardService;
 
-    @PostMapping("/save")
-    public void save(BoardDTO boardDTO) {
-        boardService.save(boardDTO);
+    @GetMapping("/save")
+    public String save() {
+        return "save";
     }
+
+    @PostMapping("/save")
+    public String save(BoardDTO boardDTO) {
+        boardService.save(boardDTO);
+        return "redirect:/list";
+    }
+
+    @GetMapping("/list")
+    public String findAll(Model model) {
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        model.addAttribute("boardList", boardDTOList);
+        return "list";
+    }
+
+
 }
--- src/main/java/com/projectnl/springboot_board/repository/BoardRepository.java
+++ src/main/java/com/projectnl/springboot_board/repository/BoardRepository.java
@@ -4,12 +4,20 @@ import com.projectnl.springboot_board.dto.BoardDTO;
 import lombok.RequiredArgsConstructor;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class BoardRepository {
     private final SqlSessionTemplate sql;
+
     public void save(BoardDTO boardDTO) {
-        sql.insert("board.save", boardDTO);
+        sql.insert("Board.save", boardDTO);
     }
+
+    public List<BoardDTO> findAll() {
+        System.out.println("findAll");
+        return sql.selectList("Board.findAll");
+    }
+
 }
